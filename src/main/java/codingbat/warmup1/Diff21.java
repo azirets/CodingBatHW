099b39c1328e6a6dff7d353fb60ebbdb477deb33
Aziret <azik927@gmail.com>
2026-07-18 20:32:33 +0300

    Exercise Diff21

---
+++ src/main/java/codingbat/warmup1/Diff21.java
@@ -0,0 +1,12 @@
+package codingbat.warmup1;
+
+public class Diff21 {
+    public int diff21(int n) {
+        if (n>=21){
+            return (n-21)*2;
+
+        } else {
+            return 21-n;
+        }
+    }
+}
