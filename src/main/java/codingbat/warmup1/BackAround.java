98843f6461001c20c5b6570aa93b119e1af38a3d
Aziret <azik927@gmail.com>
2026-07-18 22:37:30 +0300

    Exercise Back Around

---
+++ src/main/java/codingbat/warmup1/BackAround.java
@@ -0,0 +1,8 @@
+package codingbat.warmup1;
+
+public class BackAround {
+    public String backAround(String str) {
+        String last = str.substring(str.length()-1);
+        return last + str + last;
+    }
+}
