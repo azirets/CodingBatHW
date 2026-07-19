cd6b6d7c53d282f4290a27ee4dcad5c5d3b50672
Aziret <azik927@gmail.com>
2026-07-19 23:33:21 +0300

    Exercise MakeMiddle

---
+++ src/main/java/codingbat/array1/MakeMiddle.java
@@ -0,0 +1,8 @@
+package codingbat.array1;
+
+public class MakeMiddle {
+    public int[] makeMiddle(int[] nums) {
+        int mid = nums.length / 2;
+        return new int[] {nums[mid - 1], nums[mid]};
+    }
+}
