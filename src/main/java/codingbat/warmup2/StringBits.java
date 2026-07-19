a6491b51fec4767953b0e203067577f1386da1c2
Aziret <azik927@gmail.com>
2026-07-19 13:22:13 +0300

    Exercis StringBits

---
+++ src/main/java/codingbat/warmup2/StringBits.java
@@ -0,0 +1,14 @@
+package codingbat.warmup2;
+
+public class StringBits {
+    public String stringBits(String str) {
+        String result = "";
+
+        for (int i = 0; i < str.length(); i += 2) {
+            result += str.charAt(i);
+        }
+
+        return result;
+    }
+
+}
