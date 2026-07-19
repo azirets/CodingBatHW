72f05595f3c6d3e7096183e2e552714aaea1c5f7
Aziret <azik927@gmail.com>
2026-07-19 23:42:38 +0300

    Exercise SwapEnds

---
+++ src/main/java/codingbat/array1/SwapEnds.java
@@ -0,0 +1,11 @@
+package codingbat.array1;
+
+public class SwapEnds {
+    public int[] swapEnds(int[] nums) {
+        int temp = nums[0];
+        nums[0] = nums[nums.length - 1];
+        nums[nums.length - 1] = temp;
+        return nums;
+    }
+
+}
