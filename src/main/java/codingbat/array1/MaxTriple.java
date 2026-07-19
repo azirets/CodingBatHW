c1c427e78df41dcdfd4b14333762e9471c8d162b
Aziret <azik927@gmail.com>
2026-07-19 23:50:20 +0300

    Exercise MaxTriple

---
+++ src/main/java/codingbat/array1/MaxTriple.java
@@ -0,0 +1,9 @@
+package codingbat.array1;
+
+public class MaxTriple {
+    public int maxTriple(int[] nums) {
+        return Math.max(nums[0],
+                Math.max(nums[nums.length / 2], nums[nums.length - 1]));
+    }
+
+}
