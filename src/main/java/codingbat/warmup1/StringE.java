eaa6156a29a2c0703ef7fdaad03f5d6154d0b228
Aziret <azik927@gmail.com>
2026-07-19 11:32:38 +0300

    Exercise StringE

---
+++ src/main/java/codingbat/warmup1/StringE.java
@@ -0,0 +1,14 @@
+package codingbat.warmup1;
+
+public class StringE {
+    public boolean stringE(String str) {
+        int e = 0;
+
+        for(int i=0; i < str.length(); i++) {
+            if(str.charAt(i) == 'e'){
+                e++;
+            }
+        }
+        return (e >=1 && e <= 3);
+    }
+}
