430a90214289ad7c49d4a438d1e9e5d9ec104c87
Aziret <azik927@gmail.com>
2026-07-19 13:44:42 +0300

    Exercise StringYak

---
+++ src/main/java/codingbat/warmup2/StringYak.java
@@ -0,0 +1,19 @@
+package codingbat.warmup2;
+
+public class StringYak {
+    public String stringYak(String str) {
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i < str.length() - 2 &&
+                    str.charAt(i) == 'y' &&
+                    str.charAt(i + 2) == 'k') {
+                i += 2;
+            } else {
+                result += str.charAt(i);
+            }
+        }
+
+        return result;
+    }
+}
