48e5d2d188db52b59960d35e73fd5662607ed4ec
Aziret <azik927@gmail.com>
2026-07-20 01:02:33 +0300

    Exercise Front11

---
+++ src/main/java/codingbat/array1/Front11.java
@@ -0,0 +1,16 @@
+package codingbat.array1;
+
+public class Front11 {
+    public int[] front11(int[] a, int[] b) {
+        if (a.length == 0 && b.length == 0) {
+            return new int[] {};
+        }
+        if (a.length == 0) {
+            return new int[] {b[0]};
+        }
+        if (b.length == 0) {
+            return new int[] {a[0]};
+        }
+        return new int[] {a[0], b[0]};
+    }
+}
