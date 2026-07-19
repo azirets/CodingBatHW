bb41a063d96a1714069bda6add67ccc96bcb57b3
Aziret <azik927@gmail.com>
2026-07-19 23:30:30 +0300

    Exercise BiggerTwo

---
+++ src/main/java/codingbat/array1/BiggerTwo.java
@@ -0,0 +1,10 @@
+package codingbat.array1;
+
+public class BiggerTwo {
+    public int[] biggerTwo(int[] a, int[] b) {
+        if (a[0] + a[1] >= b[0] + b[1]) {
+            return a;
+        }
+        return b;
+    }
+}
