5d6adafefca367b08e90e0a450624a1d60d6019e
Aziret <azik927@gmail.com>
2026-07-19 11:15:21 +0300

    Exercise Mix Start

---
+++ src/main/java/codingbat/warmup1/MixStart.java
@@ -0,0 +1,9 @@
+package codingbat.warmup1;
+
+public class MixStart {
+    public boolean mixStart(String str) {
+        if(str.length() >= 3 && str.substring(1,3).equals("ix")){
+            return true;
+        } return false;
+    }
+}
