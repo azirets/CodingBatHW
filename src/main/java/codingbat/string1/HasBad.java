82eb7354ac23f3ee712874bd62b35ce55308d783
Aziret <azik927@gmail.com>
2026-07-19 12:29:31 +0300

    Exercise HasBad

---
+++ src/main/java/codingbat/string1/HasBad.java
@@ -0,0 +1,16 @@
+package codingbat.string1;
+
+public class HasBad {
+    public boolean hasBad(String str) {
+        if(str.length() < 3){
+            return false;
+        }
+        else if(str.substring(0,3).equals("bad")){
+            return true;
+        }else if(str.length() > 3 && str.substring(1,4).equals("bad")){
+            return true;
+        }
+        return false;
+    }
+
+}
