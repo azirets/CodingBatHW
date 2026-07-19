0aa412e8fb60564e6927927a5ed5d90e7570dad9
Aziret <azik927@gmail.com>
2026-07-19 13:29:42 +0300

    Exercise ArrayCount9

---
+++ src/main/java/codingbat/warmup2/ArrayCount9.java
@@ -0,0 +1,15 @@
+package codingbat.warmup2;
+
+public class ArrayCount9 {
+    public int arrayCount9(int[] nums) {
+        int count = 0;
+
+        for (int num : nums) {
+            if (num == 9) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
