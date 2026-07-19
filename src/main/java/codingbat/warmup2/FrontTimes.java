e2fe57f5fca67d674502b17c43e5c9343493e523
Aziret <azik927@gmail.com>
2026-07-19 13:10:47 +0300

    Exercise FrontTimes

---
+++ src/main/java/codingbat/warmup2/FrontTimes.java
@@ -0,0 +1,14 @@
+package codingbat.warmup2;
+
+public class FrontTimes {
+    public String frontTimes(String str, int n) {
+        String front = str.length() < 3 ? str : str.substring(0, 3);
+        String result = "";
+
+        for (int i = 0; i < n; i++) {
+            result += front;
+        }
+
+        return result;
+    }
+}
